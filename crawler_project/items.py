a09741346a99d51c055db830f9025a0fc22e49cd
brunsilva-jc <brunsilva.jc@gmail.com>
2022-07-21 18:28:04 -0300

    adding item container and optmizing main crawler

--- crawler_project/items.py
+++ crawler_project/items.py
@@ -6,7 +6,8 @@
 import scrapy
 
 
-class CrawlerProjectItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class GuiaMidiaItem(scrapy.Item):
+    Categoria= scrapy.Field()
+    title = scrapy.Field()
+    link = scrapy.Field()
+   
--- crawler_project/spiders/guia_midia_spider.py
+++ crawler_project/spiders/guia_midia_spider.py
@@ -1,15 +1,12 @@
 from operator import concat
 import scrapy
+from ..items import GuiaMidiaItem
 
 import scrapy
 
 class Guia_Midia_Spider(scrapy.Spider):
     name = "guia_midia"
     start_urls = [
-        #'https://www.guiademidia.com.br/agenciasdenoticias.htm',
-        #'https://www.guiademidia.com.br/sites/artes-e-cultura.htm',
-        #'https://www.guiademidia.com.br/sites/arquitetura-e-decoracao.htm',
-        #'https://www.guiademidia.com.br/sites/automobilismo.htm',
         'https://www.guiademidia.com.br/agronomia/agricultura.htm',
     ]
 
@@ -17,22 +14,27 @@ class Guia_Midia_Spider(scrapy.Spider):
        for link in response.xpath('body/main/div/nav/ul/li//a'):
             next_page = link.css('a::attr(href)').get()
             categoria = link.css('a::text').get()
-            #yield{'categoria':categoria, 'link':mylink}
-            yield response.follow(next_page, callback=self.parse_interno,
+            yield response.follow(next_page, callback=self.parse_link,
             meta={'Categoria': categoria})
            
 
-    def parse_interno(self, response):
+    def parse_link(self, response):
+        items = GuiaMidiaItem()
         for link in response.xpath('/html/body/table/tr[2]/td/table/tr[1]/td[1]/table/tr[1]/td[2]/table[2]/tr[3]//a'):
             if link.css('a::text').get() and link.css('a::attr(href)').get() is not None:
                 title = link.css('a::text').get()
                 link = link.css('a::attr(href)').get()
-                categoria = response.meta['Categoria']
-                yield  {
-                    'Categoria': categoria,
-                    'title': title,
-                    'link': link
-                    }
+
+                items['Categoria']= response.meta['Categoria']
+                items['title'] = title
+                items['link'] = link
+
+                yield items
+            
+            
+            
+                
+            
                 
 
     
