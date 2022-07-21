c9d5970f6d2f447e5eadf6f6a397bd8e9cece3a1
brunsilva-jc <brunsilva.jc@gmail.com>
2022-07-21 10:21:23 -0300

    change settings variables

--- crawler_project/settings.py
+++ crawler_project/settings.py
@@ -20,7 +20,7 @@ NEWSPIDER_MODULE = 'crawler_project.spiders'
 ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 128
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
@@ -86,3 +86,4 @@ ROBOTSTXT_OBEY = True
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+FEED_EXPORT_ENCODING = "utf-8"
